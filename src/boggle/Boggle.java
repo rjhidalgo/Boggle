7658ab04b705e003ecd2561c902f1d4d8ed831fc
Ryan Hidalgo <rhidalgo2014@gmail.com>
2018-09-13 22:59:14 -0400

    First commit

---
+++ src/boggle/Boggle.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boggle;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Ryan Hidalgo
+ */
+public class Boggle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Ouputs messages and displays a dialogue box.
+        System.out.println("Welcome to Boggle!");
+        JOptionPane.showMessageDialog(null, "Let's Play Boggle!");
+    }
+    
+}
---
+++ src/inputOutput/ReadDataFile.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inputOutput;
+
+/**
+ *
+ * @author Ryan Hidalgo
+ */
+
+//Reads data file
+public class ReadDataFile {
+    
+}
